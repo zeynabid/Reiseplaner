49232c8b596e675c53856ed344445897b045af0e
Zeyn <zeynabi@gmx.de>
2025-05-25 19:24:08 +0200

    Dockerfile für Render, mit Inhalt

--- src/main/java/de/berlin/webtech/webtech/WebtechApplication.java
+++ src/main/java/de/berlin/webtech/webtech/WebtechApplication.java
@@ -2,6 +2,9 @@ package de.berlin.webtech.webtech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WebtechApplication {
@@ -10,4 +13,14 @@ public class WebtechApplication {
 		SpringApplication.run(WebtechApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("http://localhost:5173");
+			}
+		};
+	}
 }
--- src/main/java/de/berlin/webtech/webtech/web/HelloApiController.java
+++ src/main/java/de/berlin/webtech/webtech/web/HelloApiController.java
@@ -1,4 +1,14 @@
 package de.berlin.webtech.webtech.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
+
+@RestController
 public class HelloApiController {
+
+    @GetMapping("/api/hello")
+    public Map<String, String> sayHello() {
+        return Map.of("message", "Hallo vom Backend!");
+    }
 }
